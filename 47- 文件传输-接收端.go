3ae6f6f02648ffead635d219e71575779c7bf087
shengxiangLI <2392732614@qq.com>
2021-03-24 18:40:24 +0100

    update udp

---
+++ 47- 文件传输-接收端.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func recvFile(conn net.Conn, fileName string) {
+	// 按照文件名，创建新的文件
+	f, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("os.Create err :", err)
+		return
+	}
+	defer f.Close()
+
+	// 从网络中读数据写入本地文件
+	buf := make([]byte, 4096)
+	for {
+		n, _ := conn.Read(buf)
+		if n == 0 {
+			fmt.Println("接受文件完毕")
+			return
+		}
+		// 写入本地文件，读多少写多少
+		f.Write(buf[:n])
+
+	}
+
+}
+
+func main() {
+	// 创建用于监听的socket
+	listener, err := net.Listen("tcp", "127.0.0.1:8000")
+	if err != nil {
+		fmt.Println("net.Listen err :", err)
+		return
+	}
+	defer listener.Close()
+
+	//阻塞监听
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("listener.Accept err :", err)
+		return
+	}
+	defer conn.Close()
+
+	// 获取文件名
+	buf := make([]byte, 4096)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("conn.Read err :", err)
+		return
+	}
+	fileName := string(buf[:n])
+	// 回写ok给发送端
+	conn.Write([]byte("ok"))
+
+	// 获取文件内容
+	recvFile(conn, fileName)
+
+}
---
+++ 49-UDP并发服务器.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+func main() {
+
+	// 组织一个UDP的地址结构,指定服务器的ip和port
+	srvAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:8003")
+	if err != nil {
+		fmt.Println("net.ResolveUDPAddr err :", err)
+		return
+	}
+	fmt.Println("UDP 服务器地址结构创建完成")
+	// 创建用于通信的socket
+	udpConn, err := net.ListenUDP("udp", srvAddr)
+	if err != nil {
+		fmt.Println("net.ListenUDP err :", err)
+		return
+	}
+
+	defer udpConn.Close()
+	fmt.Println("UDP 服务器通信socket创建完成")
+
+	// 读取客户端发送的数据
+	buf := make([]byte, 4096)
+	for {
+		// 返回三个值，分别是 读取到的字节数，客户端地址，error
+		n, cltAddr, err := udpConn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("udpConn.ReadFromUDP err :", err)
+			return
+		}
+		// 模拟处理数据
+		fmt.Println("来自客户端 : ", cltAddr)
+		fmt.Println("服务器读到 : ", string(buf[:n]))
+
+		go func() { // 每有一个客户端连接上来，启动一个go程写数据
+			// 回写数据给客户端
+			// 提取系统时间
+			daytime := time.Now().String()
+			_, err = udpConn.WriteToUDP([]byte(daytime), cltAddr)
+			if err != nil {
+				fmt.Println("udpConn.WriteToUDP err :", err)
+				return
+			}
+		}()
+
+	}
+
+}
